dbb10908da81e58c4583bb5a7df203dc1870928d
Muh. Wira <muhwira907@gmail.com>
2023-09-22 13:11:27 +0800

    Load emails data from data.json and display it

--- lib/pages/email_list_page.dart
+++ lib/pages/email_list_page.dart
@@ -1,8 +1,33 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:tugas_coder/widgets/email_tile.dart';
 import 'package:tugas_coder/widgets/gmail_app_bar.dart';
 
-class EmailListPage extends StatelessWidget {
+class EmailListPage extends StatefulWidget {
+  @override
+  State<EmailListPage> createState() => _EmailListPageState();
+}
+
+class _EmailListPageState extends State<EmailListPage> {
+  List<Map<String, dynamic>> emails = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadEmails();
+  }
+
+  Future<void> loadEmails() async {
+    final jsonData = await rootBundle.loadString('assets/data.json');
+    final List<dynamic> parsedJson = json.decode(jsonData);
+    final List<Map<String, dynamic>> emailList =
+        parsedJson.cast<Map<String, dynamic>>();
+    setState(() {
+      emails = emailList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,14 +37,14 @@ class EmailListPage extends StatelessWidget {
             GmailAppBar(),
           ],
           body: ListView.builder(
-            itemCount: 20,
+            itemCount: emails.length,
             itemBuilder: (context, index) => EmailTile(
-              sender: "User",
-              subject: "Subject Email",
-              content: "Content Email",
-              day: "01",
-              month: "January",
-              isImportant: false,
+              sender: emails[index]['sender'],
+              subject: emails[index]['subject'],
+              content: emails[index]['content'],
+              day: emails[index]['day'],
+              month: emails[index]['month'],
+              isImportant: emails[index]['isImportant'],
             ),
           ),
         ),
